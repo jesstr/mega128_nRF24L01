bc13f6196d3f0ffe4e56399e20d504d4d80c50e1
Pavel Cherstvov <pavel.cherstvov@ctc.local>
2013-05-31 16:49:54 +0400

    commands.h added

---
+++ mega128_nRF24L01/commands.h
@@ -0,0 +1,11 @@
+#ifndef _COMMANDS_H_
+#define _COMMANDS_H_ 1
+
+/* List of commands */
+#define COMM_OFF		0x0F
+#define COMM_ON			0x0E
+#define COMM_INSTANT	0x01
+#define COMM_FASTFLASH	0x02
+#define COMM_SLOWFLASH	0x03
+
+#endif /* _COMMANDS_H_ */
