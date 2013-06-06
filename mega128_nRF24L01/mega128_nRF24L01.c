fd5cc899f7b8f5ae4c275aea7e35cf9974ce450b
Pavel Cherstvov <pavel.cherstvov@ctc.local>
2013-06-06 18:27:45 +0400

    Leds added; Buttons delays fixed

--- mega128_nRF24L01/mega128_nRF24L01.c
+++ mega128_nRF24L01/mega128_nRF24L01.c
@@ -5,8 +5,8 @@
  *  Author: Черствов
  */
 
-/*FCU must be defined at "Properties->Toolchain->Symbols" as "F_CPU=16000000". */ 
-/* #define F_CPU 16000000 */ 
+/*FCU must be defined at "Properties->Toolchain->Symbols" as "F_CPU=8000000". */ 
+/* #define F_CPU 8000000 */ 
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -31,8 +31,9 @@ unsigned char rx_payload[RXPayloadLenght+1];
 unsigned char tx_payload[TXPayloadLenght+1]={0x01,0x02,0x03,0x00};
 
 /* Start Timer0 with divider 1024 */
-#define START_TIMER0		TCCR0|=(1<<CS02)|(1<<CS01)|(1<CS00)
-#define STOP_TIMER0			TCCR0&=~(1<<CS02)|(1<<CS01)|(1<CS00)
+#define TIMER0_DIVIDER		1024
+#define START_TIMER0		TCCR0|=(1<<CS02)|(1<<CS01)|(1<<CS00)
+#define STOP_TIMER0			TCCR0&=~(1<<CS02)|(1<<CS01)|(1<<CS00)
 
 /* Status leds registers defines */
 #define ERROR_LED_DDR	DDRD
@@ -42,6 +43,35 @@ unsigned char tx_payload[TXPayloadLenght+1]={0x01,0x02,0x03,0x00};
 #define MODE_LED_DDR	DDRD
 #define MODE_LED_PORT	PORTD
 #define MODE_LED_PIN	PD7
+
+/* Flashes per second defines */
+#define FASTFLASH_LED_FPS	15
+#define SLOWFLASH_LED_FPS	5
+#define ERROR_LED_FPS		3
+
+/* Counter of Timer ticks for mode led */
+unsigned char mode_led_counter=0;
+/* Flashes per second for mode led */
+unsigned char mode_led_fps=0;
+/* Counter of Timer ticks for error led */
+unsigned char error_led_counter=0;
+/* Flashes per second for error led */
+unsigned char error_led_fps=0;
+
+/* Leds control commands */
+#define MODE_LED_START_FAST		mode_led_fps=FASTFLASH_LED_FPS
+#define MODE_LED_START_SLOW		mode_led_fps=SLOWFLASH_LED_FPS
+#define MODE_LED_START_INSTANT	mode_led_fps=0; \
+								MODE_LED_PORT|=(1<<MODE_LED_PORT);
+#define MODE_LED_STOP			mode_led_fps=0; \
+								MODE_LED_PORT&=~(1<<MODE_LED_PIN)
+#define ERROR_LED_START			error_led_fps=FASTFLASH_LED_FPS
+#define ERROR_LED_STOP			error_led_fps=0; \
+								ERROR_LED_PORT&=~(1<<ERROR_LED_PIN)
+
+/* Delay defines */
+#define SWITCH_DELAY	_delay_ms(100)
+#define BUTTON_DELAY	_delay_ms(500)
 
 /* Marker variables */
 volatile unsigned char pwr_on; 		/* pwr_on = 1 - device is switched on
@@ -72,7 +102,7 @@ void IRQ_Init(void)
 
 
 /* Status leds initialization */
-void LEDS_Init(void)
+void Leds_Init(void)
 {
 	ERROR_LED_DDR|=(1<<ERROR_LED_PIN);
 	ERROR_LED_PORT&=~(1<<ERROR_LED_PIN);
@@ -91,10 +121,22 @@ void Timer0_Init(void)
 /* Timer0 overflow interrupt handle */
 ISR(TIMER0_OVF_vect) 
 {
+	mode_led_counter++;
+	error_led_counter++;
+	if ((mode_led_fps > 0) && (mode_led_counter >= F_CPU/TIMER0_DIVIDER/256/mode_led_fps/2)) {
+		mode_led_counter=0;
+		MODE_LED_PORT^=(1<<MODE_LED_PIN);
+	}
+	if ((error_led_fps > 0) && (error_led_counter >= F_CPU/TIMER0_DIVIDER/256/error_led_fps/2)) {
+		error_led_counter=0;
+		ERROR_LED_PORT^=(1<<ERROR_LED_PIN);
+	}
+	#if 0
 	if (ERROR_LED_PORT & (1<<ERROR_LED_PIN)) 
 		ERROR_LED_PORT&=~(1<<ERROR_LED_PIN);
 	else
 		ERROR_LED_PORT|=(1<<ERROR_LED_PIN);
+	#endif
 }
 
 
@@ -168,6 +210,14 @@ ISR(INT0_vect)
 			SPI_SendByte_Master(buff|(0x20));
 			SPI_CS1_HIGH; 
 			
+			ERROR_LED_STOP;
+			switch (lightmode){
+				case 1:	MODE_LED_START_INSTANT; break;
+				case 2:	MODE_LED_START_FAST; break;
+				case 3:	MODE_LED_START_SLOW; break;
+				default: break;
+			}
+			
 			/* LCD debug output */
 			LcdWriteCom(SECOND_STRING);
 			LcdWriteString("OK  ");
@@ -186,6 +236,8 @@ ISR(INT0_vect)
 			SPI_CS1_HIGH; 
 			
 			tx_error=1;
+			MODE_LED_STOP;
+			ERROR_LED_START;
 			
 			/* LCD debug output */
 			LcdWriteCom(SECOND_STRING);
@@ -216,11 +268,18 @@ int main(void)
 	UART1_Init(MYUBRR);
 	SPI_Init_Master();
 	Buttons_Init();
+	Leds_Init();
+	Timer0_Init();
+	START_TIMER0;
 	
 	nRF24L01_Standby_1();
 	nRF24L01_SetRXPayloadLenght(RXPayloadLenght);
 	//nRF24L01_Receive_On();
 	sei();
+	
+	/* LCD debug output */
+	LcdWriteCom(SECOND_STRING);
+	LcdWriteString("Ready");
 	
 	/* UART debug output */
  	UART1_SendString(IntToStr(nRF24L01_GetSConfig(),16));
@@ -230,7 +289,7 @@ int main(void)
     while(1)
     {
 		/* Buttons and switches polling */
-		if (MODE_BUTTON) {
+		if (MODE_BUTTON && (lightmode != 0)) {
 			/* Checking light mode */
 			if (lightmode != LIGHT_MODES_NUMBER) 
 				lightmode++;
@@ -242,17 +301,20 @@ int main(void)
 				case 3: SendCommand(COMM_SLOWFLASH); break;
 				default: break;
 			}
+			BUTTON_DELAY;
 		}
 		else if (FLASH_BUTTON) {
 			/* Flash mode of front light while button is pressed  */
 		}
 		else if ((ON_OFF_SWITCH == 1)  && (lightmode == 0)) {
-			SendCommand(COMM_ON);
 			lightmode=DEFAULT_LIGHT_MODE;
+			SendCommand(COMM_INSTANT);
+			SWITCH_DELAY;
 		}
-		else if ((ON_OFF_SWITCH == 0) && (lightmode > 0)) {
-			SendCommand(COMM_OFF);
+		else if ((ON_OFF_SWITCH == 0) && (lightmode != 0)) {
 			lightmode=0;
+			SendCommand(COMM_OFF);
+			SWITCH_DELAY;
 		}
 		else if (SOUND_SWITCH == 1) {
 			/* Beep sound on */
@@ -260,41 +322,6 @@ int main(void)
 		else if (SOUND_SWITCH == 0) {
 			/* Beep sound off */
 		}
-		_delay_ms(250);
-		
-		#if 0
-		/* test */
-		tx_payload[0]=COMM_ON;
-		nRF24L01_SendData(tx_payload, RXPayloadLenght);
-		_delay_ms(1000);
-		tx_payload[0]=COMM_SLOWFLASH;
-		nRF24L01_SendData(tx_payload, RXPayloadLenght);
-		_delay_ms(1000);
-		tx_payload[0]=COMM_FASTFLASH;
-		nRF24L01_SendData(tx_payload, RXPayloadLenght);
-		_delay_ms(1000);
-		tx_payload[0]=COMM_INSTANT;
-		nRF24L01_SendData(tx_payload, RXPayloadLenght);
-		_delay_ms(1000);
-		tx_payload[0]=COMM_OFF;
-		nRF24L01_SendData(tx_payload, RXPayloadLenght);
-		_delay_ms(2000);
-		#endif
-		
-		#if 0
-		/* LCD debug output */
-		LcdWriteCom(FIRST_STRING);
-		LcdWriteString("        ");
-		LcdWriteCom(FIRST_STRING);
-		LcdWriteString("Sent ");
-		LcdWriteString(IntToStr(count+1,10));	
-		
-		/* UART debug output */
-		UART1_SendString(IntToStr(count+1,10));
-		UART1_SendString("\x0A\x0D");
-		
-		count++;
-		_delay_ms(1000);
-		#endif    
+		//_delay_ms(250);
 	}
 }
